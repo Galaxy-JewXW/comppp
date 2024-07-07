20c03df4ca0419b08f9afa9a09ef2a854367680e
Jew <22373498@buaa.edu.cn>
2024-07-07 19:45:04 +0800

    man

--- kobe/src/EntryType/Array1.java
+++ kobe/src/EntryType/Array1.java
@@ -9,13 +9,13 @@ public class Array1 extends FuncParam {
     public Array1(int d) {
         this.dimension = d;
         this.values = new ArrayList<>();
-        this.dimension = 1;
+        this.type = 1;
     }
 
     public Array1() { // 作为函数形参
         this.dimension = 0;
         this.values = new ArrayList<>();
-        this.dimension = 1;
+        this.type = 1;
     }
 
     public int getValue(int i) {
